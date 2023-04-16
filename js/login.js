1e8cefcd85bef5137cad093155db9b88645b8d1b
Sbnam Ahmed Siyam <sabnamahmedsiyam263#gmail.com>
2023-04-16 16:01:06 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-submit').addEventListener('click',function(){
+    const emailField=document.getElementById('input-email');
+    const email=emailField.value;
+    // console.log(email);
+    const passwordField= document.getElementById('input-password');
+    const password=passwordField.value;
+    // console.log(password);
+if(email==='sontan@bap.com' && password==='secret'){
+    console.log('valid user');
+// window.location.href='./bank.html';
+    window.location.href='bank.html'
+}
+else{
+    alert('Tui to Murkho!!! tui abar kemne amar Bank a dukte chas !!!!!');
+    console.log('invalid user');
+}
+
+
+})
